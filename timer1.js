f5dc5bd98c2232272784ce910bcc26a099f20598
Robert Beckett <robert.beckett@onion.red>
2019-10-21 17:45:59 +0000

    Removed test console.log

--- timer1.js
+++ timer1.js
@@ -3,7 +3,6 @@ const args = process.argv.slice(2);
 for (const argument of args) {
   if (!Number.isNaN(argument)  && argument >= 0) {
     setTimeout(() => {
-      console.log(argument);
       process.stdout.write('\x07');
     }, argument);
   }
